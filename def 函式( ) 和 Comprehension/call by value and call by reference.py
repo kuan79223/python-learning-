3dcb7653d4bd26353d69c184270e6043e8a40e7d
賴穎寬 <112489587+kuan79223@users.noreply.github.com>
2023-01-06 13:08:03 +0800

    Update call by value and call by reference.py

--- def 函式( ) 和 Comprehension/call by value and call by reference.py
+++ def 函式( ) 和 Comprehension/call by value and call by reference.py
@@ -1,19 +1,34 @@
-#call by value
+call by value
+
 def turbo(speed):
+  
   print('加速前速度:',speed)
   speed += 10
   return speed
-if __name__ == '__main__':
-  speed = int(input('輸入初速度:'))
-  speed = turbo(speed)
-  print('加速後的速度:',speed)
 
-#call by reference
+if __name__ == '__main__':
+  
+  speed_in = int(input('輸入初速度:'))
+  
+  after_speed = turbo(speed_in)
+  print('加速後的速度:',after_speed)
+
+
+  
+  
+  
+call by reference
+
 def turbo(listspeed):
+  
   print('加速前速度:',listspeed[0])
   listspeed[0] += 10
+
   
-s = int(input('輸入初速度:'))
-listS = [s]
-turbo(listS)
-print('加速後的速度:',listS[0])
+if __name__ == '__main__':
+  
+  speed_in = int(input('輸入初速度:'))
+
+  listS = [speed_in]
+  turbo(listS)
+  print('加速後的速度:',listS[0])
